f34545d9d27ada5c9508e8bd35292d91c9193fea
Chizbang <jamertheprogrammer@gmail.com>
2014-10-06 08:10:51 -0400

    Fixed hosts being able to join to their own session

--- api/sessionhostclient.php
+++ api/sessionhostclient.php
@@ -1,15 +1,15 @@
 <?php
 include('class/class_database.php');
-if(!isset($_SESSION)){
-	session_start();
-}
+session_start();
+
 if(!isset($_SESSION['uid'])){
+	echo json_encode(["error"=>"invalid_user"]);
 	return;
-}
-if(!isset($_POST['session'])){
+} elseif(!isset($_POST['session'])){
 	echo json_encode(["error"=>"needs_session"]);
 	return;
 }
+
 $session = $_POST['session'];
 $client = $_SESSION['uid'];
 
@@ -17,7 +17,7 @@ $fetchAllDB = new Database();
 $result = $fetchAllDB->preparedQuery("SELECT * FROM sessions WHERE sessionid = ?", array($session))->fetchAll(PDO::FETCH_ASSOC);
 if($result){
 	foreach ($result as $key) {
-		if($key['hostuid'] == ""){
+		if($key['hostuid'] == "" or $key['hostuid'] == $client){
 			$setHostQuery = $fetchAllDB->preparedQuery("UPDATE sessions SET hostuid = ? WHERE sessionid = ?", array($client, $session));
 			echo json_encode(["user_status"=>"host"]);
 		} else{
