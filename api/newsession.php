891af2ebf23fe29fc2a262c2ccc78fc8c19738f9
Chizbang <jamertheprogrammer@gmail.com>
2014-10-06 08:12:07 -0400

    Removed pointless password parts. DB Table changes pending

--- api/newsession.php
+++ api/newsession.php
@@ -1,16 +1,10 @@
 <?php
 	require_once("class/class_database.php");
-	if(!isset($_POST['data'])) {
-		echo json_encode(["error"=>"invalid_data"]);
-		return;
-	}
-	$data = $_POST['data'];
 
 	$session_id = md5(uniqid());
-	$session_password = $data['password'];
 
 	$newSessionQuery = new Database();
-	$newSessionQuery->preparedQuery("INSERT INTO `sessions` (`id`, `sessionid`, `password`, `date`, `host`, `hostuid`, `finished`) VALUES (NULL ,  ?,  ?,  '2014-09-04 00:00:00',  ?,  ?, '0');", array($session_id, $session_password, "", ""));
+	$newSessionQuery->preparedQuery("INSERT INTO `sessions` (`id`, `sessionid`, `password`, `date`, `host`, `hostuid`, `finished`) VALUES (NULL ,  ?,  '',  '2014-09-04 00:00:00',  ?,  ?, '0');", array($session_id, "", ""));
 
 	echo json_encode(["status"=>"complete", "sessionid"=>$session_id]);
 ?>
