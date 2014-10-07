b24407aca976f98c00f54489d05b69c744738994
Chizbang <jamertheprogrammer@gmail.com>
2014-10-07 18:20:06 -0400

    Utilising the new session class

--- api/newsession.php
+++ api/newsession.php
@@ -1,10 +1,8 @@
 <?php
-	require_once("class/class_database.php");
+	require_once("class/class_session.php");
 
-	$session_id = md5(uniqid());
-
-	$newSessionQuery = new Database();
-	$newSessionQuery->preparedQuery("INSERT INTO `sessions` (`id`, `sessionid`, `password`, `date`, `host`, `hostuid`, `finished`) VALUES (NULL ,  ?,  '',  '2014-09-04 00:00:00',  ?,  ?, '0');", array($session_id, "", ""));
-
-	echo json_encode(["status"=>"complete", "sessionid"=>$session_id]);
+	$session = new Session();
+	$newSession = $session->createSession();
+	
+	echo json_encode(["status"=>"complete", "sessionid"=>$newSession]);
 ?>
